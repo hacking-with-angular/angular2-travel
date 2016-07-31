19b9a20cf7068aff071cb88a990e835a191e4d53
dreamapple <2451731631@qq.com>
2016-07-31 22:41:29 +0800

    基本技能: 展示数据

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### angular2-travel
 这个分支是basics部分,在这个分支我们需要学习一些关于Angular2的基础知识部分,赶快开始吧.
 
-+ [ ] Overview(概览)
-+ [ ] Architecture(架构)
++ [x] Overview(概览)
++ [x] Architecture(架构)
 + [ ] Displaying Data(显示数据)
 + [ ] User Input(用户输入)
 + [ ] Forms(表单)
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+// 引入我们要使用的类
+import {Hero} from './hero';
 
 /*
  * 别忘记了使用@前缀
@@ -6,11 +8,41 @@ import {Component} from '@angular/core';
  */
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular2 Travel</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>My favorite hero is: {{myHero}}</h2>
+        <p>Heros:</p>
+        <ul>
+            <li *ngFor="let hero of heros">
+                {{hero.name}}
+            </li>
+        </ul>
+        <p *ngIf="heros.length > 3">There are many heros!</p>
+        `
 })
 
 /*
  * 导出这个组件,也就是一个类
  * 这里相当于组件控制器
  */
-export class AppComponent {}
+export class AppComponent {
+    //title = 'Tour of heros';
+    //myHero = 'dreamapple';
+
+    title: String; // 默认为共有
+    myHero: String;
+    heros: String[];
+
+    constructor() {
+        this.title = 'Tour of heros';
+        //noinspection TypeScriptValidateTypes
+        this.heros = [
+            new Hero(1, 'dream'),
+            new Hero(2, 'apple'),
+            new Hero(3, 'happy'),
+            new Hero(4, 'dreamapple')
+        ];
+        //noinspection TypeScriptUnresolvedVariable
+        this.myHero = this.heros[0].name;
+    }
+}
