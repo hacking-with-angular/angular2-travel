380f352053e2f98bfb99e66b5733f042bdb06b84
dreamapple <2451731631@qq.com>
2016-08-09 21:50:32 +0800

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-
-// 使用User类
-import {User} from './classes/User';
+import {FormComponent} from './form.component';
 
 /*
  * 别忘记了使用@前缀
@@ -9,23 +7,14 @@ import {User} from './classes/User';
  */
 @Component({
     selector: 'my-app',
-    //template: '<h1>My First Angular2 Travel</h1>'
-    templateUrl: 'app/templates/form.html'
+    templateUrl: 'app/templates/app.html',
+    directives: [
+        FormComponent
+    ]
 })
 
 /*
  * 导出这个组件,也就是一个类
  * 这里相当于组件控制器
  */
-export class AppComponent {
-    favoriteFruitList = ['apple', 'pear', 'banana', 'orange'];
-    user = new User('dreamapple', '2451123321@qq.com', 'Nothing is impossible!', this.favoriteFruitList[0]);
-
-    get userInfo() {
-        return JSON.stringify(this.user);
-    }
-
-    addUser() {
-        this.user = new User('', '', '');
-    }
-}
+export class AppComponent {}
---
+++ app/form.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+
+// 使用User类
+import {User} from './classes/User';
+
+/*
+ * 别忘记了使用@前缀
+ * 这里相当于组件视图
+ */
+@Component({
+    selector: 'my-form',
+    //template: '<h1>My First Angular2 Travel</h1>'
+    templateUrl: 'app/templates/form.html'
+})
+
+/*
+ * 导出这个组件,也就是一个类
+ * 这里相当于组件控制器
+ */
+export class FormComponent {
+    active: boolean = true;
+    favoriteFruitList = ['apple', 'pear', 'banana', 'orange'];
+    user = new User('dreamapple', '2451123321@qq.com', 'Nothing is impossible!', this.favoriteFruitList[0]);
+
+    get userInfo() {
+        return JSON.stringify(this.user);
+    }
+
+    addUser() {
+        this.active = false;
+        this.user = new User('', '', '', '');
+        setTimeout(() => this.active = true, 0);
+    }
+
+    submitForm() {
+        alert(this.userInfo + '已经被提交!');
+    }
+}
