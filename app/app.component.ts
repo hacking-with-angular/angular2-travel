1cfb009e7a5622d676e073c4b8df7d0163786cb1
dreamapple <2451731631@qq.com>
2016-10-22 19:43:16 +0800

    10-22 quick-start

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,6 @@
-import {Component} from '@angular/core';
-
-/*
- * 别忘记了使用@前缀
- * 这里相当于组件视图
- */
+import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular2 Travel</h1>'
+    template: '<h1>My First Angular App</h1>'
 })
-
-/*
- * 导出这个组件,也就是一个类
- * 这里相当于组件控制器
- */
-export class AppComponent {}
+export class AppComponent { }
